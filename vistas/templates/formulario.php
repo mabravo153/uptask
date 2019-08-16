1141cf9500028aad85c305f756e6a498d64ec851
miguel angel bravo <miguelbravo153@gmail.com>
2019-08-15 20:21:45 -0500

    creado template de login y create account

---
+++ vistas/templates/formulario.php
@@ -0,0 +1,37 @@
+
+<section class="fondo">
+
+<div class="contenedor-total">
+
+    <h1 class="centrar-texto">UpTask</h1>
+
+    <div class="contenedor-formulario contenedor">
+
+        <form action="post" id="formulario" class="formulario" >
+
+            <div class="campo">
+                <label for="usuario">Usuario</label>
+                <input type="text" name="user" id="usuario" placeholder="Usuario">
+            </div>
+            <div class="campo">
+                <label for="contrasena">Contraseña</label>
+                <input type="password" name="password" id="contrasena" placeholder="Contraseña" >
+            </div>
+
+            <div class="contenedor-login">
+                <div class="btn btn-amarillo">
+                    <a href="#" class="inicio-sesion" >iniciar sesión</a>
+                </div>
+                <div class="btn-texto btn">
+                    <a href="#" class="create-account" >crea una nueva cuenta</a>
+                </div>
+            </div>
+
+        </form>
+
+    </div>
+
+</div>
+
+</section>
+
